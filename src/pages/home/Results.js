8d008b4e9deb223946b7aa365e9416292418994d
alessandrodiaz <alessdg02@gmail.com>
2024-04-12 17:42:27 -0500

    Creación del componente con la información nutricional de cada apartado del alimento. Cambio del icono y nombre de la aplicación.

--- src/pages/home/Results.js
+++ src/pages/home/Results.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Progress } from "@material-tailwind/react";
+import NutritionInfoCard from "../../components/NutritionInfoCard";
+import data from "./data.json";
+import NutritionScore from "../../components/NutritionScore";
 
 function Results() {
 	const navigate = useNavigate(); // Hook de navegación
 
-	const puntaje = 9; // Aquí asigna el puntaje dinámicamente
-
-	const getColorClass = (puntaje) => {
-		if (puntaje <= 3) {
-			return "bg-red-500";
-		} else if (puntaje <= 7) {
-			return "bg-yellow-500";
+	const getColorFromScore = (score) => {
+		if (score <= 3) {
+			return "text-red-500";
+		} else if (score <= 7) {
+			return "text-yellow-500";
 		} else {
-			return "bg-green-500";
+			return "text-green-500";
 		}
 	};
 
@@ -23,92 +24,29 @@ function Results() {
 			<div className="flex justify-center items-center text-center w-full py-6 px-4">
 				<div className="flex-column justify-center items-center text-center w-full max-w-2xl py-6 px-4">
 					<h1 className="text-2xl font-bold">ANÁLISIS ALIMENTICIO</h1>
-					<p className="text-lg pt-6">Puntuación nutricional: 7/10</p>
-					<p className="text-lg">¿Debería consumirlo?: No</p>
-					<p className="pt-4">
-						Esta bebida tiene un contenido moderado en calorías y proteínas, pero es alta en grasas
-						saturadas. Aunque el sodio es bajo, la falta de fibra y las limitadas vitaminas hacen
-						que no sea la mejor opción para la persona con hipertensión y el objetivo de bajar de
-						peso
-					</p>
+					<NutritionScore score={data.alimento_general.calificacion_general} />
+					<p className="pt-4">{data.alimento_general.resumen}</p>
 				</div>
 			</div>
 
 			{/* DESCRIPCION DE LOS ITEMS DEL ALIMENTO */}
 			<div className="flex flex-column w-auto justify-center items-center pb-24">
 				<div className="grid gap-4 md:grid-cols-2 flex-column w-auto justify-center items-center px-4">
-					{/* CARD */}
-					<div className="flex p-4 flex-col justify-center text-center h-full w-full max-w-md rounded-3xl mt-4 bg-zinc-100 border shadow-lg">
-						<div className="flex flex-col items-center justify-center p-4">
-							<p className="font-bold text-xl pb-4">Carbohidratos</p>
-							<p className="text-sm">Puntuación: 8/10</p>
-
-							{/* BARRA INDICADORA */}
-							<div className="w-full h-4 bg-gray-200 rounded-full">
-								<div
-									className={`progress rounded-full h-full ${getColorClass(puntaje)}`}
-									style={{ width: `${puntaje * 10}%` }}
-								></div>
-							</div>
-
-							<p className="pt-6">
-								Proporciona 5 g de carbohidratos por porción, siendo 2% del valor diario.
-							</p>
-							<p>
-								La cantidad de carbohidratos es baja, lo cual puede ser positivo para el objetivo de
-								bajar de peso.
-							</p>
-						</div>
-					</div>
-					{/* //////////////////////////// */}
-
-					<div className="flex p-4 flex-col justify-center text-center h-full w-full max-w-md rounded-3xl mt-4 bg-zinc-100 border shadow-lg">
-						<div className="flex flex-col items-center justify-center p-4">
-							<p className="font-bold text-xl pb-4">Carbohidratos</p>
-							<p className="text-sm">Puntuación: 8/10</p>
-
-							{/* BARRA INDICADORA */}
-							<div className="w-full h-4 bg-gray-200 rounded-full">
-								<div
-									className={`progress rounded-full h-full ${getColorClass(puntaje)}`}
-									style={{ width: `${puntaje * 10}%` }}
-								></div>
-							</div>
-
-							<p className="pt-6">
-								Proporciona 5 g de carbohidratos por porción, siendo 2% del valor diario.
-							</p>
-							<p>
-								La cantidad de carbohidratos es baja, lo cual puede ser positivo para el objetivo de
-								bajar de peso.
-							</p>
-						</div>
-					</div>
-
-					{/* //////////////////////////// */}
-
-					<div className="flex p-4 flex-col justify-center text-center h-full w-full max-w-md rounded-3xl mt-4 bg-zinc-100 border shadow-lg">
-						<div className="flex flex-col items-center justify-center p-4">
-							<p className="font-bold text-xl pb-4">Carbohidratos</p>
-							<p className="text-sm">Puntuación: 8/10</p>
-
-							{/* BARRA INDICADORA */}
-							<div className="w-full h-4 bg-gray-200 rounded-full">
-								<div
-									className={`progress rounded-full h-full ${getColorClass(puntaje)}`}
-									style={{ width: `${puntaje * 10}%` }}
-								></div>
-							</div>
-
-							<p className="pt-6">
-								Proporciona 5 g de carbohidratos por porción, siendo 2% del valor diario.
-							</p>
-							<p>
-								La cantidad de carbohidratos es baja, lo cual puede ser positivo para el objetivo de
-								bajar de peso.
-							</p>
-						</div>
-					</div>
+					{/* NUTRITION INFO CARDS */}
+					{Object.entries(data).map(([key, value]) => {
+						if (key !== "alimento_general") {
+							return (
+								<NutritionInfoCard
+									key={key}
+									titulo={key.charAt(0).toUpperCase() + key.slice(1)} // Capitaliza la primera letra
+									puntaje={value.calificacion}
+									descripcion={value.descripcion}
+									porcentaje={value.recomendacion}
+								/>
+							);
+						}
+						return null;
+					})}
 				</div>
 			</div>
 		</div>
